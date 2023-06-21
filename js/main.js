1b4ec5097c5a2fbbba024418a6ccb229ddd92e24
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-06-21 11:02:42 -0700

    Creating tile colors and integers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 /*----- constants -----*/
+const COLORS = {
+    "0": "gray",
+    "1": "yellow",
+    "2": "red"
+}
 
+const ADJACENT_INTEGER_COLOR = {
+    "1": "blue",
+    "2": "green",
+    "3": "red",
+    "4": "purple",
+    "5": "maroon",
+    "6": "teal",
+    "7": "magenta",
+    "8": "black"
+}
+
+const INNER_TEXT = {
+    "1": "1",
+    "2": "2",
+    "3": "3",
+    "4": "4",
+    "5": "5",
+    "6": "6",
+    "7": "7",
+    "8": "8"
+}
 
 /*----- state variables -----*/
 
@@ -13,18 +39,18 @@
 /*----- functions -----*/
 init();
 function init() {
-  // rotate the board array 90 degrees counter-clockwise
+    // rotate the board array 90 degrees counter-clockwise
     board = [
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 0
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 1
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 2
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 3
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 4
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 5
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 6
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 7
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 8
-    [0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0],  // col 9
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 0
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 1
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 2
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 3
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 4
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 5
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 6
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 7
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 8
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  // col 9
     ];
     winner = null;
     render();
@@ -34,5 +60,15 @@ function render() {
     renderBoard();
     renderControls(); //hide/show UI elements/controls
 }
-    function renderBoard(){};
-    function renderControls(){};
+function renderBoard() {
+    board.forEach(function (colArr, colIdx) {
+        colArr.forEach(function (cellVal, rowIdx) {
+            const cellId = `c${colIdx}r${rowIdx}`;
+            const cellEl = document.getElementById(cellId);
+            cellEl.style.backgroundColor = COLORS[cellVal];
+//            cellEl.innerText = INNER_TEXT[cellVal];
+//            cellEl.innerTextColor = ADJACENT_INTEGER_COLOR[cellVal];
+        });
+    });
+};
+function renderControls() { };
